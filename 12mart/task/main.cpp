b637ad2a5f5f6e5ab5fd4c27cf430cb9c370b168
arina hasandjanova <arinahasandjanova@gmail.com>
2025-03-13 09:09:17 +0400

    semaphors

--- 12mart/task/main.cpp
+++ 12mart/task/main.cpp
@@ -4,6 +4,12 @@
 #include <cstdlib>
 #include <thread>
 
+void partial_sum(const std::vector<int>& vec, int index, long long& result) {
+    result = 0;
+    for(int i = index; i - index < vec.size()/2; ++i) {
+        result+=vec[i];
+    }
+}
 
 int main() {
     long long sum = 0;
@@ -18,7 +24,22 @@ int main() {
     }
     clock_t end = clock();
     double dur = double(end-start)/CLOCKS_PER_SEC;
-    std::cout << "1 used " << dur << " seconds\n";
+    std::cout << "sum 1: " << sum << "\nwith 1 thread used " << dur << " seconds\n";
+
+    long long res1 = 0, res2 = 0, sum2 = 0;
+    int index1 = 0;
+    int index2 = 50000;
+    clock_t start1 = clock();
+    std::thread t1(partial_sum, rand, index1, std::ref(res1));
+    std::thread t2(partial_sum, rand, index2, std::ref(res2));
+    //std::cout<<"\n\n\n\n\nM"<<res1<<"\nM"<<res2<<"\n";
+    t1.join();
+    t2.join();
+    sum2 = res1 + res2;
+    clock_t end1 = clock();
+    double dur1 = double(end1-start1)/CLOCKS_PER_SEC;
+    std::cout << "sum 2: " << sum2 << "\nwith 2 threads used " << dur1 << " seconds\n";
+
 
     return 0;
 }
