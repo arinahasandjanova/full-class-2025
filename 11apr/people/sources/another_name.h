56bfb0a25be3be50de58ee09d7dd15a98446775f
arinahasandjanova <arinahasandjanova@gmail.com>
2025-04-22 12:11:14 +0400

    mine

---
+++ 11apr/people/sources/another_name.h
@@ -0,0 +1,11 @@
+#ifndef ANOTHER_NAME_H
+#define ANOTHER_NAME_H
+
+#include <QMainWindow>
+#include <QApplication>
+class AnotherMenu:public QMainWindow{
+public:
+    AnotherMenu(QWidget * parent = 0);
+}
+
+#endif // ANOTHER_NAME_H
